e1b34c2640ceabfc58f2de24fe88825585e0e4ae
Cella Sum <cella@bigcartel.com>
2021-10-09 15:58:18 -0400

    update script

--- scripts/reader.js
+++ scripts/reader.js
@@ -1,15 +1,26 @@
-$(document).ready(function() {
-
-  $("*:not(body)").hover(function (ev) {
-    $(this).addClass('highlight');
-    $(this).click(function(e) {
-      console.log($(e.target).text());
-      var utter = new window.SpeechSynthesisUtterance("cat");
-      window.speechSynthesis.cancel();
-      window.speechSynthesis.speak(utter);
-    });
-  },function (ev) {
-    $(this).removeClass('highlight');
-    window.speechSynthesis.cancel();
+window.onload = function() {
+  document.body.addEventListener('mouseover', function(e) {
+    $(e.srcElement).addClass('highlight');
   });
-});
+
+  document.body.addEventListener('mouseout', function(e) {
+    $(e.srcElement).removeClass('highlight');
+    e.stopPropagation();
+  });
+
+  document.body.addEventListener('click', function(e) {
+    var el = e.srcElement;
+    var text;
+    var tagname = el.tagName;
+
+    if(el.tagName == "IMG") {
+      text = $(el).attr('alt') + " " + $(el).attr('title') + " " + $(el).attr('src');
+    } else {
+      text = $(el).text();
+    }
+    var utter = new window.SpeechSynthesisUtterance(text);
+    window.speechSynthesis.cancel();
+    window.speechSynthesis.speak(utter);
+    e.stopPropagation();
+  });
+}
